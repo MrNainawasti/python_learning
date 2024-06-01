570e7ef784f391100d9e9d2f16ef3524e7248e0c
mr.nainawasti <nainawastisujan088@gmail.com>
2024-06-01 13:21:26 +0545

    band name generator code

---
+++ Day1.py
@@ -0,0 +1,6 @@
+#Band_Name_Generator_Code
+print("Welcome to the band name generator")
+cityname = input("What is the name of the city you grew up in?\n")
+petname = input("What is the name of your pet?\n")
+print("Your band name could be:\n" + cityname + " " + petname)
+
