8ccf08fb91dabc42f7357e8442a7385707bbc814
mr.nainawasti <nainawastisujan088@gmail.com>
2024-11-21 21:01:45 +0545

    password manager project

--- Day29/README.md
+++ Day29/README.md
@@ -1,10 +1,30 @@
 # Password Manager Project
-This project is developed using different widgets from Tkinter. The UI allows the user to type-in website, email and password in the entry fields. The credentials entered are saved in a file "data.txt" when the user hits 'Add' button. A pop-up box is generated for confirmation of details entered which is created by using 'MessageBox Widget'. If any of the fields with '*' is left empty then also a pop-up box is generated as a warning. User can press 'Generate password' button to generate a random strong password which is automatically copied to clipboard ready to paste if necessary (using pyperclip).
+This project is developed using different widgets from Tkinter.
+The UI allows the user to type-in website, email and password in 
+the entry fields. The credentials entered are saved in a file "data.txt"
+when the user hits 'Add' button. A pop-up box is generated for confirmation
+of details entered which is created by using 'MessageBox Widget'.
+If any of the fields is left empty then also a pop-up box is generated as a warning.
+User can press 'Generate password' button to generate a random strong password 
+which is automatically copied to clipboard ready to paste if necessary (using pyperclip).
 
 
 # MessageBox Widget
-Python Tkinter – MessageBox Widget is used to display the message boxes(pop up boxes) in the python applications. The tkinter.messagebox module provides a template base class as well as a variety of convenience methods for commonly used configurations. The message boxes are modal and will return a subset of (True, False, None, OK, CANCEL, YES, NO) based on the user’s selection.
+Python Tkinter-MessageBox Widget is used to display the message boxes(pop up boxes) in the python applications.
+The tkinter.messagebox module provides a template base class as well as a variety of convenience methods for commonly
+used configurations. The message boxes are modal and will return a subset of (True, False, None, OK, CANCEL, YES, NO)
+based on the user's selection.
 
 # Pyperclip
 Pyperclip provides a cross-platform Python module for copying and pasting text to the clipboard.
 To copy text to the clipboard, pass a string to pyperclip.copy(). To paste the text from the clipboard, call pyperclip.paste() and the text will be returned as a string value.
+
+# JSON
+JSON stands for JavaScript Object Notation.
+JSON is a text format for storing and transporting data.
+JSON is "self-describing" and easy to understand.
+
+some functions of JSON:
+json.dump() - To write in json file
+json.load() - To read json file
+json.update() - To update json file
--- Day29/main.py
+++ Day29/main.py
@@ -2,31 +2,32 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator Project
 def generate_pw():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
             'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
             'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
             'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
+    
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-
+    
+    
     password_letters = [choice(letters)for _ in range(randint(8, 10))]
-
+    
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-
+    
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
-
+    
     password_list = password_letters + password_symbols + password_numbers
-
+    
     shuffle(password_list)
-
+    
     password = "".join(password_list)
-
+    
     pw_entry.delete(0, END)
     pw_entry.insert(0, password)
     pyperclip.copy(password)
@@ -34,23 +35,72 @@ def generate_pw():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_data():
-    website = website_entry.get()
+    website = website_entry.get().title()
     email = email_entry.get()
     password = pw_entry.get()
     
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+        }
+    
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops!", message="fields with '*' cannot be left empty!")
+        messagebox.showinfo(title="Oops!", message="website and password fields cannot be left empty!")
+    
     else:
-        is_ok = messagebox.askokcancel(title="website", message=f"Please confirm the details entered:\n Website:{website}\n Password:{password}")
-        
+        is_ok = messagebox.askyesno(title="Confirm Data", message=f"confirm the entered data:\nWebsite: {website}\nPassword: {password}" )
         if is_ok:
-            with open("data.txt", mode="a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+            try:
+                with open("data.json", mode="r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
             
+            except FileNotFoundError:
+                with open("data.json", mode="w") as data_file:
+                    # saving data by creating a file
+                    json.dump(new_data, data_file, indent=4)        
+            
+            else:
+                # Updating old data in new data
+                data.update(new_data)
+            
+                with open("data.json", mode="w") as data_file:
+                    # saving updated data
+                    json.dump(data, data_file, indent=4)
+            
+            finally:
+                website_entry.delete(0, END)
+                pw_entry.delete(0, END)
+                email_entry.delete(0, END)
+                email_entry.insert(0, "example@gmail.com")
+
+
+# ---------------------------- SEARCH DATA ------------------------------- #
+def search_data():
+    website = website_entry.get().title()
+    if len(website) == 0:
+        messagebox.showinfo(title="Oops!", message="Enter website name to search.")
+        
+    else:    
+        try:
+            with open("data.json", mode="r") as data_file:
+                read_data = json.load(data_file)
+                saved_data = read_data[website]
+                email = saved_data["email"]
+                password = saved_data["password"]
+                messagebox.showinfo(title=website, message=f"email: {email}\npassword: {password}")
+        
+        except FileNotFoundError:
+            messagebox.showinfo(title="Oops!", message= f"Sorry, no data found on {website}.")
+        
+        except KeyError:
+            messagebox.showinfo(title="Oops!", message= f"Sorry, no data found on {website}.")
+        
+        else:
             website_entry.delete(0, END)
-            pw_entry.delete(0, END)
-            email_entry.delete(0, END)
-            email_entry.insert(0, "example@gmail.com")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -63,19 +113,19 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 # labels
-website_label = Label(text="Website*:")
+website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 
-pw_label = Label(text="Password*:")
+pw_label = Label(text="Password:")
 pw_label.grid(row=3, column=0)
 
 # entries
-website_entry = Entry(width=40)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1)
 
 email_entry = Entry(width=40)
 email_entry.insert(0, "example@gmail.com")
@@ -90,6 +140,9 @@ pw_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", command= save_data, width=36)
 add_button.grid(row=4, column=1, columnspan=2)
+
+search_button = Button(text="Search", command=search_data, width= 14)
+search_button.grid(row=1, column=2)
 
 
 window.mainloop()
